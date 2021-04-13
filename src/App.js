9840c4c9c2f2d31b4ae661c606fff1494723b1d1
Harshana Abeyaratne <harshana.abeyaratne@gmail.com>
2021-04-13 10:20:35 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import useCarousel from 'react-zippy-carousel';
 
-function App() {
+const width = 300;
+
+const App = () => {
+  const carouselContainerRef = React.useRef();
+  const {
+    handlers,
+    listHandlers,
+    offset,
+    withAnimation,
+    slideNext,
+    slidePrev,
+    items,
+  } = useCarousel({
+    images: ['#98ddca', '#d5ecc2', '#ffd3b4', '#ffaaa7'],
+    carouselContainerRef,
+    autoplay: true,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={slideNext}>NEXT</button>
+      <button onClick={slidePrev}>PREV</button>
+      <div ref={carouselContainerRef} {...handlers} style={{ width: width, height: width, display: 'flex', overflow: 'hidden', margin: '0 auto', position: 'relative' }} className="App">
+        <ul {...listHandlers} style={{
+          display: 'flex', flexWrap: 'nowrap', listStyle: 'none', padding: 0, margin: 0, 
+          
+          transitionDuration: !withAnimation ? '0s' : '0.5s',
+          transform: `translate3d(${offset * -1}px, 0, 0)`,
+        }}>
+          {
+            items.map((i) => (<li style={{ display: 'flex', height: width, width: width, background: i }}>{JSON.stringify(i)}</li>))
+          }
+        </ul>
+      </div>
+    </>
   );
 }
 
