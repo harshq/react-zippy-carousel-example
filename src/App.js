699df76fca63a7d68df5a6d0b6538f4bb19972d1
Harshana Abeyaratne <harshana.abeyaratne@gmail.com>
2021-04-13 13:12:41 +0530

    rename prop

--- src/App.js
+++ src/App.js
@@ -12,26 +12,28 @@ const App = () => {
     withAnimation,
     slideNext,
     slidePrev,
-    items,
+    slides
   } = useCarousel({
-    images: ['#98ddca', '#d5ecc2', '#ffd3b4', '#ffaaa7'],
+    list: ['#98ddca', '#d5ecc2', '#ffd3b4', '#ffaaa7'],
     carouselContainerRef,
-    autoplay: true,
   });
 
   return (
     <>
-      <button onClick={slideNext}>NEXT</button>
-      <button onClick={slidePrev}>PREV</button>
+      <p>
+        <button onClick={slideNext}>NEXT</button>
+        <button onClick={slidePrev}>PREV</button>
+      </p>
+
       <div ref={carouselContainerRef} {...handlers} style={{ width: width, height: width, display: 'flex', overflow: 'hidden', margin: '0 auto', position: 'relative' }} className="App">
         <ul {...listHandlers} style={{
-          display: 'flex', flexWrap: 'nowrap', listStyle: 'none', padding: 0, margin: 0, 
-          
+          display: 'flex', flexWrap: 'nowrap', listStyle: 'none', padding: 0, margin: 0,
+
           transitionDuration: !withAnimation ? '0s' : '0.5s',
           transform: `translate3d(${offset * -1}px, 0, 0)`,
         }}>
           {
-            items.map((i) => (<li style={{ display: 'flex', height: width, width: width, background: i }}>{JSON.stringify(i)}</li>))
+            slides.map((i) => (<li key={i} style={{ display: 'flex', height: width, width: width, background: i }}>{JSON.stringify(i)}</li>))
           }
         </ul>
       </div>
